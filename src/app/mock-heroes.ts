f9b4aaa1a33e0a1e027e41a47245310df2f23057
Rhi88 <rhizdo_nop@hotmail.com>
2018-06-26 13:37:17 -0500

    Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero [] = [
+    { id: 1, name : 'Magneto' },
+    { id: 2, name : 'Gambit' },
+    { id: 3, name : 'Jublex' },
+    { id: 4, name : 'Titania' },
+    { id: 5, name : 'Jean' },
+    { id: 6, name : 'Profesor.X' }
+];
